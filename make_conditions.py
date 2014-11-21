37b0a8b37b9f05f9c867f8e51cab83b8c194c8d3
Andrew Ellis <a.w.ellis@gmail.com>
2014-11-21 01:00:34 +0100

    added make_conditions.py

---
+++ make_conditions.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# author: Andrew Ellis <a.w.ellis@gmail.com>
+# date: 20/11/2014
+
+from __future__ import division
+import csv, os
+
+task = 'gravity'
+
+V = {'participant_name': 'AE',
+     'participant_number': '01',
+     'session': '01',
+     'age': '99',
+     'hand': ['right', 'left'],
+     'gender': ['male', 'female'],
+     # 'task': ['gravity', 'egocentric'],
+     'side': ['left', 'right']}
+
+"""
+Setup output files
+"""
+if not os.path.isdir('participants'):
+    os.makedirs('participants')
+
+participant_dir = V['participant_number'] + '_' + V['participant_name']
+
+if not os.path.isdir('participants' + os.sep + participant_dir):
+    os.makedirs('participants' + os.sep + participant_dir)
+
+filename = 'participants' + os.sep + participant_dir + os.sep + \
+    '{0:s}_{1:s}_{2:s}'.format(str(V['participant_number']),
+                               V['participant_name'], task)
+
+
+positions = [0, -6, 6]
+adaptations = [True, False]
+beliefs = ['upright', 'tilted']
+durations = [15]
+
+conditions =[{'position': position, 'adaptation': adaptation, \
+            'belief': belief, 'duration': duration} \
+            for position in positions for adaptation in adaptations \
+            for belief in beliefs for duration in durations]
+
+for cond in conditions:
+    if cond['adaptation']:
+        cond['duration'] = 15
+    else:
+        cond['duration'] = 10
+
+from random import shuffle, seed
+
+seed(19834 * int(V['participant_number']))
+shuffle(conditions)
+
+
+column_headers = ['duration', 'position', 'adaptation', 'belief']
+with open(filename + '.csv', 'wb') as conditions_file:
+        writer = csv.writer(conditions_file, dialect='excel')
+        writer.writerow(column_headers)
+        for row in conditions:
+            writer.writerow(row.values())
+
+print("Created conditions file for participant {0}".format(participant_dir))
+
